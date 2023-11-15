b4bb72f05153efb45a43c17d89fe5490075379ec
Noreen-Ali <nooerfatimauaf199@gmail.com>
2023-11-15 08:05:26 +0500

    Initial commit

---
+++ E-cart/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Bar from './components/Bar'
+import './App.css'
+import Product from './components/Product'
+
+
+
+const App = () => {
+  return (
+    <>
+    <Bar/>
+    <Product/>
+    </>
+  )
+}
+
+export default App
---
+++ E-cart/src/components/Data.jsx
@@ -0,0 +1,86 @@
+export const items = [
+    {
+        id: 1,
+      category:  'mobiles',
+      title:  "Apple iphone 14",
+      imgSrc: 'https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg',
+      amazonLink: 'https://amzn.to/3puckZp',
+      description: 'Apple iphone 14 (12B GB) - Blue',
+      price: '89999'
+
+    },
+    {
+        id: 2,
+      category:  'laptops',
+      title:  "Apple MackBook Air Laptop",
+      imgSrc: 'https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg',
+      amazonLink: 'https://amzn.to/3r9rvhm',
+      description: 'Apple MackBook Air Laptop M1 chip, 13.3-inch/33.74 cm Retina Display,',
+      price: '89999'
+
+    },{
+        id: 1,
+      category:  'mobiles',
+      title:  "Apple iphone 14",
+      imgSrc: 'https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg',
+      amazonLink: 'https://amzn.to/3puckZp',
+      description: 'Apple iphone 14 (12B GB) - Blue',
+      price: '89999'
+
+    },{
+        id: 2,
+      category:  'laptops',
+      title:  "Apple MackBook Air Laptop",
+      imgSrc: 'https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg',
+      amazonLink: 'https://amzn.to/3r9rvhm',
+      description: 'Apple MackBook Air Laptop M1 chip, 13.3-inch/33.74 cm Retina Display,',
+      price: '89999'
+
+    },{
+        id: 2,
+      category:  'mobiles',
+      title:  "Apple iphone 11",
+      imgSrc: 'https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg',
+      amazonLink: 'https://amzn.to/44MiYi6',
+      description: 'Apple MackBook Air Laptop M1 chip, 13.3-inch/33.74 cm Retina Display,',
+      price: '89999'
+
+    },{
+        id: 2,
+      category:  'tablets',
+      title:  "Apple MackBook Air Laptop",
+      imgSrc: 'https://m.media-amazon.com/images/I/51b9LjzmPCL._SL1500_.jpg',
+      amazonLink: 'https://amzn.to/3RabsdH',
+      description: 'Apple MackBook Air Laptop M1 chip, 13.3-inch/33.74 cm Retina Display,',
+      price: '89999'
+
+    },{
+        id: 2,
+      category:  'laptops',
+      title:  "Apple MackBook Air Laptop",
+      imgSrc: 'https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg',
+      amazonLink: 'https://amzn.to/3r9rvhm',
+      description: 'Apple MackBook Air Laptop M1 chip, 13.3-inch/33.74 cm Retina Display,',
+      price: '89999'
+
+    },{
+        id: 2,
+      category:  'tablets',
+      title:  "Apple MackBook Air Laptop",
+      imgSrc: 'https://m.media-amazon.com/images/I/51b9LjzmPCL._SL1500_.jpg',
+      amazonLink: 'https://amzn.to/3r9rvhm',
+      description: 'Apple MackBook Air Laptop M1 chip, 13.3-inch/33.74 cm Retina Display,',
+      price: '89999'
+
+    },
+    {
+        id: 2,
+      category:  'laptops',
+      title:  "Apple MackBook Air Laptop",
+      imgSrc: 'https://m.media-amazon.com/images/I/71TPda7cwUL._SL1500_.jpg',
+      amazonLink: 'https://amzn.to/3r9rvhm',
+      description: 'Apple MackBook Air Laptop M1 chip, 13.3-inch/33.74 cm Retina Display,',
+      price: '89999'
+
+    },
+]
